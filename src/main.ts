c7005a2f33e4bb3e485010538dc3fb87812dd1d2
nilam-pathak <npathak.p@imerit.net>
2023-01-09 22:10:04 +0530

    basic CRUD operations added

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // creating the application instance
   const app = await NestFactory.create(AppModule);
+
+  // running the server and listening on a port
   await app.listen(3000);
 }
 bootstrap();
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  // TaskService is a provider in the TaskModule
+  // which has been injected in the TaskController via the @Injectable() decorator
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    // when filter parameters are provided in the request
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    }
+
+    // when no filter parameter is provided in the request
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    // extracts and returns the task with the corresponding id provided in the request parameter
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    // creates and returns a new task with the request body parameters
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    // extracts the task with the corresponding id provided in the request parameter
+    // then updates the status of the task based on the value provided in the request body
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    // extracts and deletes a task with its corresponding id provided in the request parameter
+    this.tasksService.deleteTask(id);
+  }
 }
