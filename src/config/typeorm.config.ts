cbe149513060cd6df3b36655350aa7d0bc49f881
nilam23 <nilampathak72@gmail.com>
2023-04-19 19:07:29 +0530

    type added to typeorm configs

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 // defining the TypeORM configs
 export const typeOrmConfigs = (): TypeOrmModuleOptions => {
-  const host = process.env.DB_HOST;
-  const port = parseInt(process.env.DB_PORT);
-  const username = process.env.DB_USER;
-  const password = process.env.DB_PASSWORD;
-  const database = process.env.DB_NAME;
+  const host: string = process.env.DB_HOST;
+  const port: number = parseInt(process.env.DB_PORT);
+  const username: string = process.env.DB_USER;
+  const password: string = process.env.DB_PASSWORD;
+  const database: string = process.env.DB_NAME;
 
   const dbConfigs: TypeOrmModuleOptions = {
     type: 'postgres',
